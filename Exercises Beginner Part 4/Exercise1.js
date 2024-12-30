b7ad570284d1b98744f250b66ff7d1bc02228565
Kavin Kumaran <gk.kavin05@gmail.com>
2024-12-29 17:43:46 -0700

    Add files via upload

---
+++ Exercises Beginner Part 4/Exercise1.js
@@ -0,0 +1,14 @@
+/*
+  Exercise 1:
+  -----------
+  Given a year, report if its a leap year.
+*/
+
+const leapYear = (num) => num % 4 === 0;
+
+console.log(leapYear('2014'));
+console.log(leapYear('2015'));
+console.log(leapYear('2016'));
+console.log(leapYear('2017'));
+console.log(leapYear('2018'));
+console.log(leapYear('2019'));
